b74b4439a493ae8c717527a577ea7d121fef2a85
Lucas Rêgo <lucasrego@gmail.com>
2015-07-08 00:39:17 -0300

    login e logotipo (nao esta sendo usado)

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Maria Gata</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <!--base css styles-->
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+
+        <!--page specific css styles-->
+
+        <!--flaty css styles-->
+        <link rel="stylesheet" href="css/flaty.css">
+        <link rel="stylesheet" href="css/flaty-responsive.css">
+
+        <link rel="shortcut icon" href="img/favicon.png">
+    </head>
+    <body class="login-page">
+
+        <!-- BEGIN Main Content -->
+		
+		<div class="login-wrapper">
+            <!-- BEGIN Login Form -->
+            <form id="form-login" action="index.html" method="get">
+                <h3>Maria Gata</h3>
+                <hr/>
+                <div class="form-group">
+                    <div class="controls">
+                        <input type="text" placeholder="Usuário" class="form-control" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="controls">
+                        <input type="password" placeholder="Senha" class="form-control" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="controls">
+                        <button type="submit" class="btn btn-primary form-control">Acessar</button>
+                    </div>
+                </div>
+                <hr/>
+                
+            </form>
+            <!-- END Login Form -->
+
+			
+        </div>
+        <!-- END Main Content -->
+
+
+        <!--basic scripts-->
+        <script src="assets/jquery/jquery-2.1.4.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+		<script src="assets/gritter/js/jquery.gritter.min.js"></script>
+		
+		 <!--flaty scripts-->
+		<script src="js/base.js"></script>
+		
+    </body>
+</html>
